8a60e07b84b7ae011edd11ff7b426b513a2a5974
David Rodrigues <david.rodrigues@rentalhost.net>
2022-06-22 15:18:28 -0300

    First commit;

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+  id("java")
+  id("org.jetbrains.intellij") version "1.6.0"
+}
+
+group = "net.rentalhost"
+version = "1.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+  version.set("2021.3")
+  type.set("IC") // Target IDE Platform
+
+  plugins.set(listOf(/* Plugin Dependencies */))
+}
+
+tasks {
+  // Set the JVM compatibility versions
+  withType<JavaCompile> {
+    sourceCompatibility = "11"
+    targetCompatibility = "11"
+  }
+
+  patchPluginXml {
+    sinceBuild.set("213")
+    untilBuild.set("223.*")
+  }
+
+  signPlugin {
+    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+    privateKey.set(System.getenv("PRIVATE_KEY"))
+    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+  }
+
+  publishPlugin {
+    token.set(System.getenv("PUBLISH_TOKEN"))
+  }
+}
