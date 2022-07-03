da66c4f47e105049551833a2a72acdaf2fe3e88c
David Rodrigues <david.rodrigues@rentalhost.net>
2022-07-02 22:36:36 -0300

    [Refactor] VariableService: works only with VariableImpl;

--- src/main/java/net/rentalhost/plugins/services/VariableService.kt
+++ src/main/java/net/rentalhost/plugins/services/VariableService.kt
@@ -1,21 +1,17 @@
 package net.rentalhost.plugins.services
 
-import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.php.lang.psi.elements.impl.VariableImpl
 
 object VariableService {
-    fun getLeafReference(element: PsiElement): LeafPsiElement? {
-        if (element !is VariableImpl)
-            return null
-
+    fun getLeafReference(element: VariableImpl): LeafPsiElement? {
         return with(PsiTreeUtil.skipWhitespacesAndCommentsBackward(element)) {
             if (this is LeafPsiElement && this.text == "&") this
             else null
         }
     }
 
-    fun isLeafReference(element: PsiElement): Boolean =
+    fun isLeafReference(element: VariableImpl): Boolean =
         getLeafReference(element) != null
 }
