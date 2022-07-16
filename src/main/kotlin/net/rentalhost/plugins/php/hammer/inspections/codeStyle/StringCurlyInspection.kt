b086c5bc49e0c328fb5bbeb136f8a11130ec322d
David Rodrigues <david.rodrigues@rentalhost.net>
2022-07-15 21:24:36 -0300

    [Changed] StringCurlyInspection: quick-fix implemented;

--- src/main/kotlin/net/rentalhost/plugins/php/hammer/inspections/codeStyle/StringCurlyInspection.kt
+++ src/main/kotlin/net/rentalhost/plugins/php/hammer/inspections/codeStyle/StringCurlyInspection.kt
@@ -6,6 +6,8 @@ import com.intellij.psi.PsiElementVisitor
 import com.jetbrains.php.lang.inspections.PhpInspection
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression
 import com.jetbrains.php.lang.psi.elements.impl.VariableImpl
+import net.rentalhost.plugins.services.FactoryService
+import net.rentalhost.plugins.services.LocalQuickFixService
 import net.rentalhost.plugins.services.ProblemsHolderService
 
 class StringCurlyInspection: PhpInspection() {
@@ -23,7 +25,11 @@ class StringCurlyInspection: PhpInspection() {
             ProblemsHolderService.registerProblem(
                 problemsHolder,
                 element,
-                "Variable must have curly braces."
+                "Variable must have curly braces.",
+                LocalQuickFixService.SimpleReplaceQuickFix(
+                    "Wrap with curly braces",
+                    FactoryService.createCurlyVariable(problemsHolder.project, element.name)
+                )
             )
         }
     }
---
+++ src/test/resources/inspections/codeStyle/StringCurlyInspection/default.fixed.php
@@ -0,0 +1,17 @@
+<?php
+
+$dummy = "{$a}";
+
+$dummy = "dummy {$a} dummy";
+
+// Not applicable:
+
+$dummy = '$a';
+
+$dummy = "\$a";
+
+$dummy = "dummy \$a dummy";
+
+$dummy = "dummy ${a} dummy";
+
+$dummy = "dummy {$a} dummy";
