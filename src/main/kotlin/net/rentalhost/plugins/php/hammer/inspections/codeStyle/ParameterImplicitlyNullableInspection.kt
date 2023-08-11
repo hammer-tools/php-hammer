be2e25fb2ae04f8edfdb7bb327c0a6a4d5e1a53a
David Rodrigues <david.proweb@gmail.com>
2023-08-10 22:39:21 -0300

    [Added] ParameterImplicitlyNullableInspection: added option to specify the replacement nullable format between short and long (as default) (fixes #19);

--- src/main/kotlin/net/rentalhost/plugins/php/hammer/inspections/codeStyle/ParameterImplicitlyNullableInspection.kt
+++ src/main/kotlin/net/rentalhost/plugins/php/hammer/inspections/codeStyle/ParameterImplicitlyNullableInspection.kt
@@ -2,6 +2,7 @@ package net.rentalhost.plugins.php.hammer.inspections.codeStyle
 
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.refactoring.suggested.createSmartPointer
+import com.intellij.util.xmlb.annotations.OptionTag
 import com.jetbrains.php.config.PhpLanguageLevel
 import com.jetbrains.php.lang.inspections.PhpInspection
 import com.jetbrains.php.lang.psi.elements.Parameter
@@ -9,11 +10,17 @@ import com.jetbrains.php.lang.psi.elements.impl.ParameterImpl
 import com.jetbrains.php.lang.psi.elements.impl.PhpTypeDeclarationImpl
 import com.jetbrains.php.lang.psi.resolve.types.PhpType
 import com.jetbrains.php.lang.psi.visitors.PhpElementVisitor
+import net.rentalhost.plugins.php.hammer.inspections.enums.OptionNullableTypeFormat
 import net.rentalhost.plugins.php.hammer.services.FactoryService
+import net.rentalhost.plugins.php.hammer.services.OptionsPanelService
 import net.rentalhost.plugins.php.hammer.services.ProblemsHolderService
 import net.rentalhost.plugins.php.hammer.services.QuickFixService
+import javax.swing.JComponent
 
 class ParameterImplicitlyNullableInspection : PhpInspection() {
+  @OptionTag
+  var nullableTypeFormat: OptionNullableTypeFormat = OptionNullableTypeFormat.LONG
+
   override fun buildVisitor(problemsHolder: ProblemsHolder, isOnTheFly: Boolean): PhpElementVisitor = object : PhpElementVisitor() {
     override fun visitPhpParameter(element: Parameter) {
       if (element !is ParameterImpl ||
@@ -27,6 +34,10 @@ class ParameterImplicitlyNullableInspection : PhpInspection() {
         declaredType.types.contains(PhpType._MIXED))
         return
 
+      if (nullableTypeFormat == OptionNullableTypeFormat.SHORT &&
+        declaredType.types.size >= 2)
+        return
+
       val elementPointer = element.createSmartPointer()
 
       ProblemsHolderService.instance.registerProblem(
@@ -38,13 +49,23 @@ class ParameterImplicitlyNullableInspection : PhpInspection() {
 
           if (elementLocal.typeDeclaration != null) {
             (elementLocal.typeDeclaration ?: return@simpleInline)
-              .replace(FactoryService.createParameterType(problemsHolder.project, (elementLocal.typeDeclaration ?: return@simpleInline).text + "|null"))
+              .replace(
+                FactoryService.createParameterType(
+                  problemsHolder.project,
+                  getParameterReplacement((elementLocal.typeDeclaration ?: return@simpleInline).text)
+                )
+              )
           }
           else {
             val parameterComplex = FactoryService.createComplexParameter(problemsHolder.project, elementLocal.text)
 
             with(parameterComplex.typeDeclaration as PhpTypeDeclarationImpl) {
-              replace(FactoryService.createParameterType(problemsHolder.project, "$text|null"))
+              replace(
+                FactoryService.createParameterType(
+                  problemsHolder.project,
+                  getParameterReplacement(text)
+                )
+              )
             }
 
             elementLocal.replace(FactoryService.createComplexParameterDoctypeCompatible(problemsHolder.project, parameterComplex.text))
@@ -54,5 +75,28 @@ class ParameterImplicitlyNullableInspection : PhpInspection() {
     }
   }
 
-  override fun getMinimumSupportedLanguageLevel(): PhpLanguageLevel = PhpLanguageLevel.PHP800
+  private fun getParameterReplacement(text: String?): String {
+    if (nullableTypeFormat == OptionNullableTypeFormat.LONG)
+      return "$text|null"
+
+    return "?$text"
+  }
+
+  override fun getMinimumSupportedLanguageLevel(): PhpLanguageLevel = PhpLanguageLevel.PHP710
+
+  override fun createOptionsPanel(): JComponent {
+    return OptionsPanelService.create { component: OptionsPanelService ->
+      component.delegateRadioCreation { radioComponent: OptionsPanelService.RadioComponent ->
+        radioComponent.addOption(
+          "Prefer short format", nullableTypeFormat === OptionNullableTypeFormat.SHORT,
+          "Your nullable types will look like: <code>?int</code>"
+        ) { nullableTypeFormat = OptionNullableTypeFormat.SHORT }
+
+        radioComponent.addOption(
+          "Prefer long format", nullableTypeFormat === OptionNullableTypeFormat.LONG,
+          "Your nullable types will look like: <code>int|null</code>"
+        ) { nullableTypeFormat = OptionNullableTypeFormat.LONG }
+      }
+    }
+  }
 }
--- src/test/kotlin/net/rentalhost/plugins/php/hammer/inspections/codeStyle/ParameterImplicitlyNullableInspectionTestCase.kt
+++ src/test/kotlin/net/rentalhost/plugins/php/hammer/inspections/codeStyle/ParameterImplicitlyNullableInspectionTestCase.kt
@@ -1,7 +1,15 @@
 package net.rentalhost.plugins.php.hammer.inspections.codeStyle
 
+import net.rentalhost.plugins.php.hammer.inspections.enums.OptionNullableTypeFormat
 import net.rentalhost.plugins.php.hammer.services.TestCase
 
 class ParameterImplicitlyNullableInspectionTestCase : TestCase() {
   fun testAll(): Unit = testInspection(ParameterImplicitlyNullableInspection::class.java)
+
+  fun testFormatShort(): Unit =
+    testInspection(
+      ParameterImplicitlyNullableInspection::class.java,
+      "nullableTypeFormatShort",
+      { it.nullableTypeFormat = OptionNullableTypeFormat.SHORT }
+    )
 }
